83911ab6e2ac16dbb0e8801eda6bec2e30d0e273
Rezabh1122 <rezabollhasany11@gmail.com>
2025-09-13 16:17:55 +0330

    Add files via upload

---
+++ dic/task_1_1.py
@@ -0,0 +1,18 @@
+animals = {'loin':'شیر' , 'tiger':'ببر' , 'elephant':'فیل' , 'dag':'سگ' , 'cat':'گربه' ,
+ 'snake':'مار' , 'whale':'نهنگ' , 'pig':'خوک' , 'spider':'عنکبوت' , 'fly': 'مگس' , 'bee':'زنبور'}
+
+
+
+bady = {'arm':'بازو' , 'knee':'زانو' , 'head':'سر' , 'neck':'گردن' , 'shoulder':'شانه' ,  
+'face':'صورت' , 'hair':'مو' , 'eye':'چشم' , 'nose':'بینی' , 'mouth':'دهان' , 'bottom':'باسن'}
+
+
+
+clothes = {'dress':'مانتو' , 'coat':'کت' , 'shorts':'شورتک' , 'shirt':'پیرهن' , 'jeans':'شلوار جین' , 
+ 'tshirt':'تیشرت' , 'suit':'کت شلوار' , 'skirt':'دامن' , 'top':'تاب'}
+
+
+ 
+print(bady['knee'])
+print(animals['pig'])
+print(clothes['dress'])
